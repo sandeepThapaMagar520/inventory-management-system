a0a7629612d056d6665aaf8a8f745e30b0646862
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-09-06 22:03:08 +0545

    structure

--- src/main/java/com/springboot/ims/controller/ImsController.java
+++ src/main/java/com/springboot/ims/controller/ImsController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ImsContoller {
+public class ImsController {
     @GetMapping("/")
-    public String home(){
-        return "index.html";
+    public String index(){
+        return "index";
     }
 
 }
